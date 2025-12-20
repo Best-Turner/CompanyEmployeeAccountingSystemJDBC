6ce78a818ad1e83bbea3cdb3899c08939413479d
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-12-20 22:13:05 +0300

    feat: добавитьь логирование для DepartmentDao и HibernateSessionFactoryUtil.
    * Добавил логирование в HibernateSessionFactoryUtil при получении SessioFactory и его закрытии
    * Добил логирование для CRUD операций в DepartmentDao

--- src/main/java/org/example/dao/DepartmentDao.java
+++ src/main/java/org/example/dao/DepartmentDao.java
@@ -1,9 +1,10 @@
 package org.example.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.exception.DepartmentNotFoundException;
 import org.example.model.Department;
 import org.example.model.Employee;
-import org.example.util.DatabaseConnection;
 import org.example.util.HibernateSessionFactoryUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -13,7 +14,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+
 public class DepartmentDao {
+    private static final Logger LOGGER = LogManager.getLogger(DepartmentDao.class);
 
     public Department save(Department department) {
 //        final String sql = "INSERT INTO department (name, budget) VALUES (?, ?)";
@@ -50,7 +53,7 @@ public class DepartmentDao {
             session.persist(department);
             transaction.commit();
         } catch (Exception e) {
-            System.out.println("Ошибка сохранения департамента " + e.getMessage());
+            LOGGER.error("Ошибка сохранения департамента", e);
             throw e;
         }
         return department;
@@ -72,20 +75,29 @@ public class DepartmentDao {
 //            throw new RuntimeException(e);
 //        }
 //        return Optional.ofNullable(department);
+        LOGGER.debug("Попытка получения департамента по ID: {}", id);
         Department department = null;
         try (Session session = HibernateSessionFactoryUtil.getSession()) {
+            LOGGER.trace("Сессия Hibernate получена");
             Transaction transaction = session.beginTransaction();
+            LOGGER.trace("Транзакция начата");
             try {
+                LOGGER.debug("Выполнение find для Department c ID: {}", id);
                 department = session.find(Department.class, id);
+                LOGGER.trace("Получили Department по ID: {}", id);
                 transaction.commit();
+                LOGGER.trace("Транзакция завершилась удачно - commit");
             } catch (Exception e) {
-                System.err.println("Ошибка получение департамента по ID = " + id + " " + e.getMessage());
+                LOGGER.error("Ошибка получение департамента по ID: {}", id, e);
+                LOGGER.trace("Попытка закрыть транзакцию");
                 if (transaction != null && transaction.isActive()) {
                     transaction.rollback();
+                    LOGGER.debug("Откат транзакции из-за ошибки");
                 }
                 throw new RuntimeException(e.getMessage());
             }
         }
+        LOGGER.debug("Возвращаем результат для ID {}: {}", id, department != null ? "найден" : "не найден");
         return Optional.ofNullable(department);
     }
 
@@ -102,17 +114,24 @@ public class DepartmentDao {
 //        } catch (SQLException e) {
 //            throw new RuntimeException(e);
 //        }
+        LOGGER.debug("Попытка получения списка всех департаментов");
         try (Session session = HibernateSessionFactoryUtil.getSession()) {
+            LOGGER.trace("Сессия Hibernate получена");
             Transaction transaction = session.beginTransaction();
+            LOGGER.trace("Транзакция начата");
             try {
                 List<Department> departments = session.createQuery("FROM Department d ORDER BY d.id", Department.class).list();
                 transaction.commit();
+                LOGGER.trace("Транзакция успешна - commit");
+                LOGGER.info("Список департаментов получен!");
                 return departments;
             } catch (Exception e) {
+                LOGGER.error("Ошибка получения списка департаментов, {}", e.getMessage(), e);
+                LOGGER.trace("Попытка откатить транзакцию");
                 if (transaction != null && transaction.isActive()) {
                     transaction.rollback();
+                    LOGGER.debug("Откат транзакции из-за ошибки");
                 }
-                System.err.println("Ошибка получения списка департаментов " + e.getMessage());
                 throw new RuntimeException(e.getMessage());
             }
         }
@@ -134,25 +153,34 @@ public class DepartmentDao {
 //        } catch (SQLException e) {
 //            throw new RuntimeException(e);
 //        }
+        LOGGER.debug("Попытка удалить Department по ID: {}", id);
         boolean result = false;
         try (Session session = HibernateSessionFactoryUtil.getSession()) {
+            LOGGER.trace("Получение Hibernate сессии");
             Transaction transaction = session.beginTransaction();
+            LOGGER.trace("Транзакция начата");
             try {
+                LOGGER.debug("Выполнение запроса на удаление департамента с ID: {}", id);
                 int deletedRow = session.createQuery("DELETE FROM Department WHERE id = :id")
                         .setParameter("id", id).executeUpdate();
                 if (deletedRow == 1) {
                     transaction.commit();
+                    LOGGER.trace("Удаление успешно. Транзакция закрыта");
                     result = true;
                 } else {
+                    LOGGER.warn("Удаление не удалось");
                     transaction.rollback();
+                    LOGGER.trace("Откат транзакции");
                 }
             } catch (Exception e) {
+                LOGGER.error("Ошибка при удалении департамента по ID: {}, {}", id, e.getMessage(), e);
                 if (transaction != null && transaction.isActive()) {
+                    LOGGER.trace("Откат транзакции из-за ошибки");
                     transaction.rollback();
                 }
-                System.err.println("Ошибка удаления департамента по ID = " + id + " " + e.getMessage());
             }
         }
+        LOGGER.info("Результат удаления по ID: {} - {}", id, result ? "удален" : "не удален");
         return result;
     }
 
@@ -183,9 +211,13 @@ public class DepartmentDao {
 //        } catch (SQLException e) {
 //            throw new RuntimeException(e);
 //        }
+        LOGGER.debug("Попытка обновить Department по ID: {}", id);
         try (Session session = HibernateSessionFactoryUtil.getSession()) {
+            LOGGER.trace("Получение Hibernate сессии");
             Transaction transaction = session.beginTransaction();
+            LOGGER.trace("Транзакция начата");
             try {
+                LOGGER.debug("Выполнение запроса на обновление");
                 int affectedRow = session.createQuery("UPDATE Department d " +
                                                       "SET d.name = :name, d.budget = :budget" +
                                                       " WHERE d.id = :id")
@@ -194,15 +226,18 @@ public class DepartmentDao {
                         .setParameter("id", id)
                         .executeUpdate();
                 if (affectedRow != 1) {
+                    LOGGER.warn("Департамент с ID: {} не найден", id);
                     throw new DepartmentNotFoundException("Департамент с ID = " + id + " не найден");
                 }
                 transaction.commit();
+                LOGGER.trace("Обновление успешно. Транзакция закрыта");
                 return true;
             } catch (Exception e) {
                 if (transaction != null && transaction.isActive()) {
+                    LOGGER.debug("Откат транзакции из-за ошибки {}", e.getMessage());
                     transaction.rollback();
                 }
-                System.err.println("Ошибка обновления департамента с ID = " + id + " " + e.getMessage());
+                LOGGER.error("Ошибка обновления департамента с ID: {}, {}", id, e.getMessage(), e);
                 throw new RuntimeException("Ошибка обновления департамента", e);
             }
         }
@@ -239,15 +274,21 @@ public class DepartmentDao {
 //        }
 //        return countEmployees;
 
+        LOGGER.debug("Попытка получить количество работников в Department по ID: {}", id);
         try (Session session = HibernateSessionFactoryUtil.getSession()) {
-            Transaction transaction = session.beginTransaction();
+            LOGGER.trace("Получение Hibernate сессии");
+            session.beginTransaction();
+            LOGGER.trace("Транзакция начата");
             Long countEmployees = session.createQuery("SELECT count(e) " +
                                                       "FROM Employee e " +
                                                       "WHERE e.department.id = :id", Long.class)
                     .setParameter("id", id).uniqueResult();
+            session.getTransaction().commit();
+            LOGGER.debug("Успешно. Транзакция закрыта");
+            LOGGER.info("Получили количество работников");
             return countEmployees != null ? countEmployees : 0L;
         } catch (Exception e) {
-            System.err.println("Ошибка получения сотрудников по департаменту");
+            LOGGER.error("Ошибка получения сотрудников по департаменту c ID: {}, {}", id, e.getMessage(), e);
             throw new RuntimeException("Не удалось получить количество сотрудников", e);
         }
     }
@@ -277,22 +318,31 @@ public class DepartmentDao {
 //            throw new RuntimeException(e);
 //        }
 //        return employees;
+
         List<Employee> employees = null;
+        LOGGER.debug("Попытка получить данные работников в Department по ID: {}", id);
         try (Session session = HibernateSessionFactoryUtil.getSession()) {
+            LOGGER.trace("Получение Hibernate сессии");
             Transaction transaction = session.beginTransaction();
+            LOGGER.trace("Транзакция начата");
             try {
+                LOGGER.debug("Выполнение запроса получения данных сотрудников");
                 Query query = session.createQuery("FROM Employee e " +
                                                   "WHERE e.department.id = :departmentId " +
                                                   "ORDER BY e.id");
                 employees = query.setParameter("departmentId", id).list();
                 transaction.commit();
+                LOGGER.debug("Успешно. Транзакция закрыта");
             } catch (Exception e) {
                 if (transaction != null && transaction.isActive()) {
+                    LOGGER.debug("Откат транзакции из-за ошибки {}", e.getMessage());
                     transaction.rollback();
                 }
-                System.err.println("Ошибка при получении сотрудников департамента с ID  = " + id + "\n" + e.getMessage());
+                LOGGER.error("Ошибка получения сотрудников по департаменту c ID: {}, {}", id, e.getMessage(), e);
+                throw e;
             }
         }
+        LOGGER.info("Получили данные ссотрудников по ID: {} департамента", id);
         return employees == null ? Collections.emptyList() : employees;
     }
 }
--- src/main/java/org/example/util/HibernateSessionFactoryUtil.java
+++ src/main/java/org/example/util/HibernateSessionFactoryUtil.java
@@ -1,12 +1,15 @@
 package org.example.util;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.model.Department;
 import org.example.model.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 public class HibernateSessionFactoryUtil {
+    public static final Logger LOGGER = LogManager.getLogger(HibernateSessionFactoryUtil.class);
 
     private static SessionFactory sessionFactory;
 
@@ -16,10 +19,10 @@ public class HibernateSessionFactoryUtil {
                     .addAnnotatedClass(Department.class)
                     .addAnnotatedClass(Employee.class)
                     .buildSessionFactory();
-            System.out.println("Соединение установлено");
+            LOGGER.info("Соединение установлено");
         } catch (Exception e) {
-            System.out.println("что тот пошло не так");
-            e.printStackTrace();
+            LOGGER.error("Ошибка инициализации SessionFactory", e);
+            throw new ExceptionInInitializerError("Не удалось инициализировать Hibernate SessionFactory");
         }
     }
 
@@ -28,12 +31,15 @@ public class HibernateSessionFactoryUtil {
 
 
     public static Session getSession() {
+        LOGGER.info("Получение сессии");
         return sessionFactory.openSession();
     }
 
     public static void shutDown() {
+        LOGGER.info("Попытка закрыть SessionFactory");
         if (sessionFactory != null) {
             sessionFactory.close();
+            LOGGER.info("SessionFactory закрыта");
         }
     }
 }
