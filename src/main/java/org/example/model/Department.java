413bc56f1b5d53e36c5c7d132bc4ceb65e9b0edc
Aleksandr Trofimov <shurikx6881@gmail.com>
2025-12-23 22:36:14 +0300

    добавить валидацию полей в Department

--- src/main/java/org/example/model/Department.java
+++ src/main/java/org/example/model/Department.java
@@ -1,6 +1,7 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,8 +10,13 @@ import java.util.List;
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+    @NotBlank(message = "Имя не может быть пустым")
+    @Size(message = "Название должно быть от 2 до 50 символов", min = 2, max = 50)
     private String name;
+    @NotNull(message = "Значение не может быть пустым")
+    @DecimalMin(message = "Бюджет не может быть равным 0", value = "0.01", inclusive = true)
+    @DecimalMax(message = "Значение не может быть больше 1_000_000_000", value = "1000000000", inclusive = true)
     private Double budget;
 
     @OneToMany(mappedBy = "department")
